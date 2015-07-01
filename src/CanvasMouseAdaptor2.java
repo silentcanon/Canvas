7de2f74487479260b5702ec5c6f4802211d35f81
noMoon <nomoonx@gmail.com>
2015-07-01 17:08:41 -0400

    draw straight line, square, circle.

--- src/CanvasMouseAdaptor2.java
+++ src/CanvasMouseAdaptor2.java
@@ -34,18 +34,18 @@ public class CanvasMouseAdaptor2 extends MouseAdapter {
         } else if (Setting.getCurrentTool() == Tool.Rectangle) {
             startX = e.getX();
             startY = e.getY();
-            Rectangle rect = new Rectangle(startX,startY,1,1);
+            Rectangle rect = new Rectangle(startX, startY, 1, 1);
             GlassPanel glassPanel = Environ.getGlassPanel();
             glassPanel.setShape(rect);
-        } else if(Setting.getCurrentTool() == Tool.Oval) {
+        } else if (Setting.getCurrentTool() == Tool.Oval) {
             startX = e.getX();
             startY = e.getY();
-            Ellipse2D oval = new Ellipse2D.Float(startX,startY,1,1);
+            Ellipse2D oval = new Ellipse2D.Float(startX, startY, 1, 1);
             Environ.getGlassPanel().setShape(oval);
-        } else if (Setting.getCurrentTool()==Tool.Line){
-            startX=e.getX();
-            startY=e.getY();
-            Line2D line=new Line2D.Float(startX,startY,startX,startY);
+        } else if (Setting.getCurrentTool() == Tool.Line) {
+            startX = e.getX();
+            startY = e.getY();
+            Line2D line = new Line2D.Float(startX, startY, startX, startY);
             Environ.getGlassPanel().setShape(line);
         }
     }
@@ -60,23 +60,43 @@ public class CanvasMouseAdaptor2 extends MouseAdapter {
         }
         int x = e.getX();
         int y = e.getY();
-        int nx,ny,height,width;
-        width = Math.abs(x-startX);
-        height = Math.abs(y-startY);
+        int nx, ny, height, width;
+        width = Math.abs(x - startX);
+        height = Math.abs(y - startY);
         nx = startX;
         ny = startY;
-        if(y < startY)
+        if (y < startY)
             ny = y;
-        if(x < startX)
+        if (x < startX)
             nx = x;
         GlassPanel glassPanel = Environ.getGlassPanel();
         Shape s = null;
-        if(currentTool == Tool.Rectangle) {
-            s = new Rectangle(nx,ny,width-1,height-1);
-        } else if(currentTool == Tool.Oval) {
-            s = new Ellipse2D.Float(nx,ny,width-1,height-1);
-        }else if (currentTool==Tool.Line){
-            s=new Line2D.Float(startX,startY,x,y);
+        if (currentTool == Tool.Rectangle) {
+            if (Setting.getShiftStatus()) {
+                //draw square
+                int edge = width > height ? width : height;
+                s = new Rectangle(nx, ny, edge - 1, edge - 1);
+            } else {
+                s = new Rectangle(nx, ny, width - 1, height - 1);
+            }
+        } else if (currentTool == Tool.Oval) {
+            if (Setting.getShiftStatus()) {
+                //draw circle
+                int edge = width > height ? width : height;
+                s = new Ellipse2D.Float(nx, ny, edge - 1, edge - 1);
+            } else {
+                s = new Ellipse2D.Float(nx, ny, width - 1, height - 1);
+            }
+        } else if (currentTool == Tool.Line) {
+            if (Setting.getShiftStatus()) {
+                if (height > width) {
+                    s = new Line2D.Float(startX, startY, startX, y);
+                } else {
+                    s = new Line2D.Float(startX, startY, x, startY);
+                }
+            } else {
+                s = new Line2D.Float(startX, startY, x, y);
+            }
         }
         glassPanel.setShape(s);
 
@@ -93,28 +113,47 @@ public class CanvasMouseAdaptor2 extends MouseAdapter {
         }
         int x = e.getX();
         int y = e.getY();
-        int nx,ny,height,width;
-        width = Math.abs(x-startX);
-        height = Math.abs(y-startY);
+        int nx, ny, height, width;
+        width = Math.abs(x - startX);
+        height = Math.abs(y - startY);
         nx = startX;
         ny = startY;
-        if(y < startY)
+        if (y < startY)
             ny = y;
-        if(x < startX)
+        if (x < startX)
             nx = x;
         Pattern p = null;
         Environ.getGlassPanel().clear();
-        if(currentTool == Tool.Rectangle) {
-            p = new RectangleComponent(nx,ny,width,height);
-        } else if(currentTool == Tool.Oval) {
-            p = new OvalPattern(nx,ny,width,height);
-        }else if(currentTool==Tool.Line){
-            p=new LinePattern(nx,ny,width,height,startX,startY,x,y);
+        if (currentTool == Tool.Rectangle) {
+            if (Setting.getShiftStatus()) {
+                //draw square
+                int edge = width > height ? width : height;
+                p = new RectangleComponent(nx, ny, edge, edge);
+            } else {
+                p = new RectangleComponent(nx, ny, width, height);
+            }
+        } else if (currentTool == Tool.Oval) {
+            if (Setting.getShiftStatus()) {
+                //draw circle
+                int edge = width > height ? width : height;
+                p = new OvalPattern(nx, ny, edge, edge);
+            } else {
+                p = new OvalPattern(nx, ny, width, height);
+            }
+        } else if (currentTool == Tool.Line) {
+            if (Setting.getShiftStatus()) {
+                if (height > width) {
+                    p = new LinePattern(nx, ny, width, height, startX, startY, startX, y);
+                } else {
+                    p = new LinePattern(nx, ny, width, height, startX, startY, x, startY);
+                }
+            } else {
+                p = new LinePattern(nx, ny, width, height, startX, startY, x, y);
+            }
         }
-        ((JLayeredPane)e.getSource()).add(p,Setting.getLayer());
+        ((JLayeredPane) e.getSource()).add(p, Setting.getLayer());
 
     }
-
 
 
 //    @Override
@@ -124,7 +163,6 @@ public class CanvasMouseAdaptor2 extends MouseAdapter {
 //        int y = e.getY();
 //        System.out.println(x + " " + y);
 //    }
-
 
 
 }
